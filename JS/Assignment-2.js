068a9e352d3f3eff7348ef0cc0a235722c28d53e
VaTSal1245 <shahvatsal523@gmail.com>
2026-03-24 12:38:23 +0530

    fixes

--- JS/Assignment-2.js
+++ JS/Assignment-2.js
@@ -54,37 +54,37 @@
 
 
 //4: Product Filter & Transform
-const products=[
-    {Productname: 'Mobile-Phone-1', price: 10000},
-    {Productname: 'Vegetables', price: 408},
-    {Productname: 'gadget', price: 1000} 
-]
+// const products=[
+//     {Productname: 'Mobile-Phone-1', price: 10000},
+//     {Productname: 'Vegetables', price: 408},
+//     {Productname: 'gadget', price: 1000} 
+// ]
 
-const filterP=products.filter(p=>p.price>1000)
-console.log(filterP)
-const discount=products.map(p=> ({
-    ...p,
-    discountedPrice: p.price-p.price*0.1
-}));
-console.log(discount)
+// const filterP=products.filter(p=>p.price>1000)
+// console.log(filterP)
+// const discount=products.map(p=> ({
+//     ...p,
+//     discountedPrice: p.price-p.price*0.1
+// }));
+// console.log(discount)
 
 
 //5: Dynamic Number Processor
-// let array=[1,2,3,4,5,6,7,8,9,10]
-// const even=array.filter(num=>(num%2==0))
-// const odd=array.filter(num=>(num%2!=0))
+let array=[1,2,3,4,5,6,7,8,9,10]
+const even=array.filter(num=>(num%2==0))
+const odd=array.filter(num=>(num%2!=0))
 
-// console.log(`Even number array: ${even}`)
-// console.log(`Odd number array: ${odd}`)
+console.log(`Even number array: ${even}`)
+console.log(`Odd number array: ${odd}`)
 
-// let sumEven=0
-// let sumOdd=0
-// even.forEach(element => {
-//     sumEven=sumEven+element;
-// });
-// odd.forEach(element => {
-//     sumOdd=sumOdd+element;
-// });
+let sumEven=0
+let sumOdd=0
+even.forEach(element => {
+    sumEven=sumEven+element;
+});
+odd.forEach(element => {
+    sumOdd=sumOdd+element;
+});
 
-// console.log(`Sum of even number: ${sumEven}`)
-// console.log(`Sum of odd number: ${sumOdd}`)
+console.log(`Sum of even number: ${sumEven}`)
+console.log(`Sum of odd number: ${sumOdd}`)
