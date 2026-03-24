71ce12d2ba4822129531cc677d1be48a2d1892cf
VaTSal1245 <shahvatsal523@gmail.com>
2026-03-24 09:37:04 +0530

    Updated the files of assignment-1

--- JS/Assignment-1.js
+++ JS/Assignment-1.js
@@ -1,28 +1,85 @@
 //Assignment 1: Smart Discount Calculator
-let username='Vatsal Shah'
-let age=18
-let purchaseAmt=6000.00
+// let username='Vatsal Shah'
+// let age=18
+// let purchaseAmt=6000.00
 
-if(age<18){
-    purchaseAmt=purchaseAmt-purchaseAmt*0.10
-    console.log(`Hi ${username}! You received a total discount of 10%. Final amount: ₹${purchaseAmt}`)
-}
-else if(age<18 && purchaseAmt>5000){
-    purchaseAmt=purchaseAmt-purchaseAmt*0.15
-    console.log(`Hi ${username}! You received a total discount of 15%. Final amount: ₹${purchaseAmt}`)
-}
-else if(age>=60 && purchaseAmt>5000){
-    purchaseAmt=purchaseAmt-purchaseAmt*0.25
-    console.log(`Hi ${username}! You received a total discount of 25%. Final amount: ₹${purchaseAmt}`)
-}
-else if(age>=60){
-    purchaseAmt=purchaseAmt-purchaseAmt*0.20
-    console.log(`Hi ${username}! You received a total discount of 20%. Final amount: ₹${purchaseAmt}`)
-}
-else if(purchaseAmt>5000){
-    purchaseAmt=purchaseAmt-purchaseAmt*0.05
-    console.log(`Hi ${username}! You received a total discount of 5%. Final amount: ₹${purchaseAmt}`)
-}
-else{
-    console.log(`Hi ${username}! You received a total discount of 0%. Final amount: ₹${purchaseAmt}`)
-}
+// if(age<18){
+//     purchaseAmt=purchaseAmt-purchaseAmt*0.10
+//     console.log(`Hi ${username}! You received a total discount of 10%. Final amount: ₹${purchaseAmt}`)
+// }
+// else if(age<18 && purchaseAmt>5000){
+//     purchaseAmt=purchaseAmt-purchaseAmt*0.15
+//     console.log(`Hi ${username}! You received a total discount of 15%. Final amount: ₹${purchaseAmt}`)
+// }
+// else if(age>=60 && purchaseAmt>5000){
+//     purchaseAmt=purchaseAmt-purchaseAmt*0.25
+//     console.log(`Hi ${username}! You received a total discount of 25%. Final amount: ₹${purchaseAmt}`)
+// }
+// else if(age>=60){
+//     purchaseAmt=purchaseAmt-purchaseAmt*0.20
+//     console.log(`Hi ${username}! You received a total discount of 20%. Final amount: ₹${purchaseAmt}`)
+// }
+// else if(purchaseAmt>5000){
+//     purchaseAmt=purchaseAmt-purchaseAmt*0.05
+//     console.log(`Hi ${username}! You received a total discount of 5%. Final amount: ₹${purchaseAmt}`)
+// }
+// else{
+//     console.log(`Hi ${username}! You received a total discount of 0%. Final amount: ₹${purchaseAmt}`)
+// }
+
+//Assignment 2: Login Validation System
+// let storedUsername='vatsal'
+// let enterUsername='vatsal'
+// let storedPassword='abc'
+// let enterPassword='abc'
+
+// if(storedUsername!=enterUsername && storedPassword!=enterPassword){
+//     console.log("Both are incorrect")
+// }
+// else if(storedUsername === enterUsername && storedPassword!=enterPassword){
+//     console.log("Incorrect Password")
+// }
+// else if(storedUsername != enterUsername && storedPassword === enterPassword){
+//     console.log("Incorrect Username")
+// }
+// else {
+//     console.log("Login Successfull!")
+// }
+
+//Assignment 3: Number Analyzer Tool
+// let number = 4
+
+// if (number >= 0) {
+//     if (number % 2 == 0) {
+//         console.log(`The number ${number} is positive and even.`)
+//     }
+//     else {
+//         console.log(`The number ${number} is positive and odd.`)
+//     }
+// }
+// else if(number<0){
+//     if (number % 2 == 0) {
+//         console.log(`The number ${number} is negative and even.`)
+//     }
+//     else {
+//         console.log(`The number ${number} is negative and odd.`)
+//     }
+// }
+// else{
+//     console.log(`Invalid input`)
+// }
+
+//Assignment 4: Shopping Eligibility Checker
+// let isMember = true
+// let cartValue=2000
+// if(isMember==true && cartValue>1000){
+//     console.log('You are eligible for free delivery!')
+// }
+// else if(cartValue>2000){
+//     console.log('You are eligible for free delivery!')
+// }
+// else{
+//     console.log('Not Eligible for free delivery. Please add more items!')
+// }
+
+//
